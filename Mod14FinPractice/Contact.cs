29eee25b7fdc0aa72ef3130f112ab7b3c33c1983
JJibbz <vkazancev322@gmail.com>
2024-03-02 22:15:30 +0700

    Добавьте файлы проекта.

---
+++ Mod14FinPractice/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod14FinPractice
+{
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) 
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; }
+        public string LastName { get; }
+        public long PhoneNumber { get; }
+        public string Email { get; }
+    }
+}
